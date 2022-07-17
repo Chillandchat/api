5a9481b38c808f086b0c62fb4c7a3c68f5d39021
Alvin Cheng <chengalvin333@gmail.com>
2022-07-17 17:58:06 +1000

    Update docs

--- src/utils/uptime.ts
+++ src/utils/uptime.ts
@@ -1,5 +1,12 @@
 import debug from "./debug";
 
+/**
+ * This is the up time function, this function insures that the server is running at all times.
+ * Due to the heroku server SIGTERM signal, which turns off the server every 30 minutes. This function will enable 100% up-time
+ * 
+ * @note This function has no arguments
+ */
+
 const upTime = (): void => {
   setTimeout(() => {
     debug.log("Heroku up time reset.");
