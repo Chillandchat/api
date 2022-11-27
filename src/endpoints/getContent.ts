27c89f87f29ec3c148b50caf03f2564b53667380
Alvin Cheng <chengalvin333@gmail.com>
2022-11-27 15:27:42 +1100

    Fix 404 issue

--- src/endpoints/getContent.ts
+++ src/endpoints/getContent.ts
@@ -7,13 +7,14 @@ import debug from "../utils/debug";
  * This is the get content endpoint, this endpoint will fetch and send the file to the client.
  *
  * @experimental
- * @type {POST} This endpoint is a POST typed endpoint.
+ * @type {GET} This endpoint is a GET typed endpoint.
  * @param {string} id The id of the content from the database.
  * @param {string} user The user who sent the file.
  */
 
 const getContent = (req: Request, res: Response, _next: NextFunction): void => {
-  const path: string = `${__dirname}/user-content/${req.query.user}/${req.query.id}.`;
+  const path: string = `${__dirname}/../user-content/${req.query.user}/${req.query.id}.`;
+  let data: string;
 
   if (req.query.key !== String(process.env.KEY)) {
     res.status(401).send("Invalid api key.");
@@ -21,17 +22,19 @@ const getContent = (req: Request, res: Response, _next: NextFunction): void => {
   }
 
   try {
-    if (fs.existsSync(path + "gif")) {
-      res.sendFile(path + "gif");
+    if (fs.existsSync(`${path}gif`)) {
+      data = fs.readFileSync(`${path}gif`, "base64").toString();
       debug.log(`Sent ${req.query.id}.gif`);
-    } else if (fs.existsSync(path + "img")) {
-      res.sendFile(path + "img");
-      debug.log(`Sent ${req.query.id}.img`);
+    } else if (fs.existsSync(`${path}png`)) {
+      data = fs.readFileSync(`${path}png`, "base64").toString();
+      debug.log(`Sent ${req.query.id}.png`);
     } else {
       res
         .status(400)
         .send(`${req.query.id} was not found in ${req.query.user}'s folder.`);
     }
+
+    res.status(200).send(data);
   } catch (err: unknown) {
     res.status(500).send(`SERVER ERROR: ${err}`);
     debug.error(err);
