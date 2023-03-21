f71c6853dbea37a212a8977623a95ffef7153040
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-03-21 16:39:47 +1100

    Create upload token endpoint

---
+++ src/endpoints/uploadToken.ts
@@ -0,0 +1,58 @@
+import { NextFunction, Request, Response } from "express";
+
+import notification from "../schema/notificationSchema";
+import debug from "../utils/debug";
+import { NotificationSchemaType } from "../utils";
+
+/**
+ * This is the upload token endpoint, this endpoint will create or modify the token database entry.
+ *
+ * @experimental
+ * @param {string} id The id of the new/old entry.
+ * @param {string} token The new token of the notification entry.
+ * @type {POST} The is a post typed endpoint.
+ */
+
+const uploadToken = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+): Promise<void> => {
+  if (req.query.key !== String(process.env.KEY)) {
+    res.status(401).send("Invalid api key.");
+    return;
+  }
+  try {
+    await notification
+      .findOne({ id: req.body.id })
+      .exec()
+      .then(
+        async (notificationInstance: NotificationSchemaType): Promise<void> => {
+          if (notificationInstance === null) {
+            await new notification({
+              id: req.body.id,
+              token: req.body.token,
+            })
+              .save()
+              .then((): void => {
+                debug.log("Saved new notification entry.");
+                res.status(201).send("Created new notification entry.");
+              });
+          } else {
+            notification
+              .findOneAndUpdate({ id: req.body.id }, { token: req.body.token })
+              .exec()
+              .then((): void => {
+                debug.log("Updated notification entry.");
+                res.status(200).send("Updated entry successfully.");
+              });
+          }
+        }
+      );
+  } catch (err: unknown) {
+    res.status(500).send(`SERVER ERROR: ${err}`);
+    debug.error(err);
+  }
+};
+
+export default uploadToken;
