e97cccd2593d450ce1de604fd346ffd8eb33317a
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-06-12 13:18:32 +1000

    Update sanitizeDirectory.js

--- src/utils/sanitizeDirectory.js
+++ src/utils/sanitizeDirectory.js
@@ -6,13 +6,15 @@
 import sanitize from "sanitize-filename";
 
 async function sanitizeDirectory(inputDirectory) {
-  const regex = /^(.*\/)?([\w-]+)\.(\w+)$/;
-  let [fullPath, directory, fileName, extension] = regex.exec(inputDirectory);
+  const PARSER_REGEX = /^(.*\/)?([\w-]+)\.(\w+)$/;
+
+  if (inputDirectory.includes("/../"))
+    throw new Error(`File path: ${inputDirectory} is not allowed.`);
+
+  let [fullPath, directory, fileName, extension] =
+    PARSER_REGEX.exec(inputDirectory);
 
   fileName = sanitize(fileName);
-
-  if (directory.includes(/\/?\.\.\//g.source))
-    throw new Error(`File path: ${directory} is not allowed.`);
 
   if (extension !== "webp" && extension !== "gif")
     throw new Error(`File extension: ${extension} is not allowed.`);
